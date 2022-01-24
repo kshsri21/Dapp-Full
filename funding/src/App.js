af061932a92795eab57b339bfebef69433c21b48
srivastavakshitijprofessional@gmail.com <srivastavakshitijprofessional@gmail.com>
2022-01-24 20:09:28 +0530

    Dapp -1

---
+++ funding/src/App.js
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
+
+function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  });
+
+  const [account, setAccount] = useState(null);
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+
+      if (provider) {
+        provider.request({ method: "eth_requestAccounts" });
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+        });
+      } else {
+        console.error("Please install MetaMask!", error);
+      }
+      // if (window.ethereum) {
+      //   provider = window.ethereum;
+      //   try {
+      //     await provider.enable();
+      //   } catch {
+      //     console.error("User is not allowed");
+      //   }
+      // } else if (window.web3) {
+      //   provider = window.web3.currentProvider;
+      // } else if (!process.env.production) {
+      //   provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      // }
+    };
+
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
+
+  // console.log(web3Api.web3);
+  return (
+    <>
+      <div class="card text-center">
+        <div class="card-header">Funding</div>
+        <div class="card-body">
+          <h5 class="card-title">Balance: 20 ETH </h5>
+          <p class="card-text">
+            Account : {account ? account : "not connected"}
+          </p>
+          {/* <button
+            type="button"
+            class="btn btn-success"
+            onClick={async () => {
+              const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+              });
+              console.log(accounts);
+            }}
+          >
+            Connect to metamask
+          </button> */}
+          &nbsp;
+          <button type="button" class="btn btn-success ">
+            Transfer
+          </button>
+          &nbsp;
+          <button type="button" class="btn btn-primary ">
+            Withdraw
+          </button>
+        </div>
+        <div class="card-footer text-muted">Code Eater</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+//npm install --save react-scripts@4.0.3
